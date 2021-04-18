c06a8c4820ee0e03f261066a95e576bf5b89252b
Christopher Nguyen <christophernguyen41@gmail.com>
2021-04-18 13:16:29 -0400

    [ADD] get dominant five colors through k means

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from PIL import Image
 import numpy as np
+import cv2
+from sklearn.cluster import KMeans
+
 class Colorizer():
-    def __init__(self):
-        self   
-    def create_image(self):
+    CLUSTERS = None    
+    def __init__(self, clusters = 5):
+        self.CLUSTERS = clusters
+
+    # Converts original image into grayscale image
+    def create_grayscale_image(self):
+        # Open image
         image = Image.open("images.jpeg")
+
+        # Convert to rgb image
         image_rgb = image.convert("RGB")
+
+        # Get width and height of image
         width, height = image.size
         gray = 0
+
+        # Initialize numpy array
         data = np.zeros((height, width), dtype=np.uint8)
+
+        # Take each rgb value for each pixel and calculate the corresponding grayscale pixel and store in array
         for i in range(width):
             for j in range(height):
                 rgb_pixel_value = image_rgb.getpixel((i,j))
@@ -17,11 +32,38 @@ class Colorizer():
                 b = rgb_pixel_value[2]
                 gray = (0.21 * r) + (0.72 * g) + (0.07 * b)
                 data[j][i] = gray        
+        
+        #Create image from calculated grayscale pixels and return
         grayscale_image = Image.fromarray(data)
         grayscale_image.save('new.png')
         return grayscale_image
 
+    # Gets the five most representative colors of the image using k means clustering
+    def getDominantColors(self):
+         # Get the image
+        img = cv2.imread("images.jpeg")
+        
+        # Convert to rgb image
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # reshapes 3 channel array into total number of pixels * 3 with three columns representing each rgb value
+        img = img.reshape((img.shape[0] * img.shape[1], 3))
+
+        # cluster the pixels into 5 seperate clusters
+        k = KMeans(n_clusters = self.CLUSTERS)
+        k.fit(img)
+
+        # cluster gets our dominant colors through the centroids
+        self.COLORS = k.cluster_centers_
+        
+        # Convert colors to int and return
+        return self.COLORS.astype(int)
+
+
+
 if __name__ == '__main__':
     imageColorizer = Colorizer()
-    grayscale_image = imageColorizer.create_image()
-    grayscale_image.show()
+    grayscale_image = imageColorizer.create_grayscale_image()
+    #grayscale_image.show()
+    domColors = imageColorizer.getDominantColors()
+    print(domColors)
