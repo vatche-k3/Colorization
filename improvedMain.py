16a4ccd665eac80b8270e67f3876052503b12831
vatche-k3 <55327813+vatche-k3@users.noreply.github.com>
2021-04-25 19:37:38 -0400

    [ ADD ] One hot encoding

--- improvedMain.py
+++ improvedMain.py
@@ -175,7 +175,6 @@ class Colorizer():
         half = (int)(width/2)
 
         leftSide = {}
-        indexValue = 0
 
         # Get each patch on the left side of the image
         for i in range(half):
@@ -191,10 +190,10 @@ class Colorizer():
                         break   
                 leftSide[(i,j)] = (list(patch), list(encoding)) 
         
-        # print(leftSide)
-        # print("The end")
-        # print("--- %s seconds ---" % (time.time() - start_time))
-        return leftSide
+        print(leftSide)
+        print("The end")
+        print("--- %s seconds ---" % (time.time() - start_time))
+        return 0
     
     # Function creates a new image out of the five dominant colors
     def create_representative_image(self, colors):
@@ -212,15 +211,6 @@ class Colorizer():
         test_image.save('improvedRep.png')
         return test_image
     
-    # Performs the softmax function
-    def softmax(self):
-        logits = [2.0, 1.0, 0.1]
-        exps = [np.exp(i) for i in logits]
-        print(exps)
-        sum_of_exps = sum(exps)
-        softmax = [j/sum_of_exps for j in exps]    
-        print(softmax) 
-
     # Grabs the closest representative color to current color and returns
     def closestColor(self,colors,color):
         colors = np.array(colors)
@@ -236,10 +226,7 @@ if __name__ == '__main__':
     grayscale_image = imageColorizer.create_grayscale_image()
     # grayscale_image.show()
     domColors = imageColorizer.getDominantColors()
-    # print(domColors)
+    print(domColors)
     testImage = imageColorizer.create_representative_image(domColors)
     # testImage.show()
-    # inputData = imageColorizer.getVectorX(domColors)
-    # print("input:")
-    # print(inputData)
-    imageColorizer.softmax()
+    inputData = imageColorizer.getVectorX(domColors)
